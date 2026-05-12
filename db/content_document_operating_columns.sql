c2e274279de80f52a3a4f6dc163c896718bda18f
Beomballa <wjdqjatnwkd@gmail.com>
2026-05-12 14:11:04 +0900

    26.05.12 DB 스키마 SQL 파일 제거

--- db/content_document_operating_columns.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE ct_document
-    ADD COLUMN status VARCHAR(20) NOT NULL DEFAULT 'DRAFT' AFTER board_type,
-    ADD COLUMN public_yn VARCHAR(1) NOT NULL DEFAULT 'Y' AFTER status,
-    ADD COLUMN pinned_yn VARCHAR(1) NOT NULL DEFAULT 'N' AFTER public_yn;
-
-CREATE INDEX idx_ct_document_status ON ct_document (status);
-CREATE INDEX idx_ct_document_public_yn ON ct_document (public_yn);
-CREATE INDEX idx_ct_document_pinned_yn ON ct_document (pinned_yn);
